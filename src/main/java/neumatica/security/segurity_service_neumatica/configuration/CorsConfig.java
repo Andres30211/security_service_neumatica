1fac612e1dfeabf0d2b7b17516bbab138abbcda2
Andres30211 <felifit27@gmail.com>
2026-08-20 11:13:35 -0500

    Primer push del micro-servicio de seguridad para neumatica

--- src/main/java/neumatica/security/segurity_service_neumatica/configuration/CorsConfig.java
+++ src/main/java/neumatica/security/segurity_service_neumatica/configuration/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://neumatica-crm.netlify.app/"));
 
         configuration.setAllowedMethods(
                 List.of(
