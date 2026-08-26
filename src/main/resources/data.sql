f3ea69f2310966d92551cf4c2858197792cc3e86
Andres30211 <felifit27@gmail.com>
2026-08-26 08:25:16 -0500

    Desarrollando la interfas de administración de usuarios

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,11 @@ ON CONFLICT (name) DO NOTHING;
 INSERT INTO roles (id, name)
 VALUES (gen_random_uuid(), 'ROLE_ADMIN')
 ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO roles (id, name)
+VALUES (gen_random_uuid(), 'ROLE_VENDEDOR')
+ON CONFLICT (name) DO NOTHING;
+
+INSERT INTO roles (id, name)
+VALUES (gen_random_uuid(), 'ROLE_PUBLICISTA')
+ON CONFLICT (name) DO NOTHING;
